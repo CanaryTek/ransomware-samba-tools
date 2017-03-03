5bcf406f742d172206628c755c2c61e153ff5388
Kuko Armas <kuko@canarytek.com>
2017-03-03 21:33:36 +0000

    Rename bait files in setup script

--- tools/setup_samba_honeypots.sh
+++ tools/setup_samba_honeypots.sh
@@ -2,8 +2,10 @@
 # Kuko Armas <kuko@canarytek.com>
 
 # Bait files dir. You can put your own files here
-# **NOTE:** Make sure you add this files in the honeypot_re in the fail2ban samba-filter file
 bait_files_dir="/root/bait-files"
+# Detection string
+# **NOTE:** Make sure you add this files in the honeypot_re in the fail2ban samba-filter file
+bait_string="ShahZeZ6"
 # This should be a name that windows finds first
 honey_folder="____Secret_Data____"
 # Get shares path from samba config
@@ -13,7 +15,12 @@ for share in $shares; do
 	echo "Seting honeypot in $share"
 	mkdir -p "/$share/$honey_folder"
 	# Copy files
-	cp $bait_files_dir/* /$share/$honey_folder/
+	for fullfile in $bait_files_dir/*; do
+		filename=$(basename "$fullfile")
+		extension="${filename##*.}"
+		filename="${filename%.*}"
+		cp $fullfile /$share/$honey_folder/$filename-$bait_string.$extension
+	done
 	# Everyone needs write permissions here...
 	chmod -R 777 "/$share/$honey_folder"
 done
